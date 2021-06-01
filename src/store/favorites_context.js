697a08d8bb2f14a7afe4235c41c5a5d1ebf954e3
Alejandro Rodriguez <alerrdguez88@gmail.com>
2021-06-01 13:48:11 -0500

    CreateContext_1

--- src/store/favorites_context.js
+++ src/store/favorites_context.js
@@ -1,3 +1,34 @@
-import {createContext} from 'react';
+import {createContext, useState} from 'react';
 
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0
+});
+
+
+function FavoritesContextProvider(props){
+
+  const [userFavorites, setuserFavorites] = useState([]);
+  
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length
+  };
+
+  function addFavoriteHnadler(favoriteMeetup) {
+    setuserFavorites(userFavorites.concat(favoriteMeetup));
+  }
+  function removeFavoriteHnadler() {
+    
+  }
+  function itemsFavoriteHnadler(params) {
+    
+  }
+
+  return <FavoritesContext.Provider value={context}>
+    {props.children}
+  </FavoritesContext.Provider>
+}
 
